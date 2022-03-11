5248e27ee0cb9844eb3e7025dff3d7881e89d0cb
Corvu-s <roomtemp72@gmail.com>
2022-03-10 20:30:01 -0500

    cleaned up product view

--- pages/productDetails.js
+++ pages/productDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import { useAppContext } from "../state/AppContext";
 import { useRouter } from "next/router";
-import { Button, Input, Spacer, Card } from "@nextui-org/react";
+import { Input, Spacer, Card, Button, Text, Row } from "@nextui-org/react";
 import NavBar from "../components/NavBar";
 
 export default function productDetails({ data }) {
@@ -14,19 +14,44 @@ export default function productDetails({ data }) {
   return (
     <div>
       <NavBar />
-
-      <p>Product Details </p>
-      <p>{productDetails.productName}</p>
       <p>Category: {productDetails.category}</p>
-      <p>Description: {productDetails.description}</p>
-      <p>Colors: {productDetails.color}</p>
-      <p>${productDetails.price}</p>
-      <p>{productDetails.quantity} Left</p>
-      <Button
-        onClick={() => dispatch({ type: "ADD_TO_CART", data: productDetails })}
-      >
-        Add to cart
-      </Button>
+      <div className="flex justify-center">
+        <Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
+          <Card.Body css={{ p: 0 }}>
+            <Card.Image
+              objectFit="cover"
+              src="/placeholder-image.png"
+              width="100%"
+              height={300}
+              alt="default"
+            />
+            <Row wrap="wrap" justify="space-between">
+              <Text b>{productDetails.productName}</Text>
+              <Text b>${productDetails.price}</Text>
+              <Text css={{ color: "$accents4", fontWeight: "$semibold" }}>
+                {productDetails.quantity} Left
+              </Text>
+              <Row>
+                <Text b> Color(s): {productDetails.color}</Text>
+              </Row>
+              <Row>
+                <Text> {productDetails.description}</Text>
+              </Row>
+            </Row>
+          </Card.Body>
+          <Card.Footer>
+            <Row wrap="wrap" justify="space-between">
+              <Button
+                onClick={() =>
+                  dispatch({ type: "ADD_TO_CART", data: productDetails })
+                }
+              >
+                Add to cart
+              </Button>
+            </Row>
+          </Card.Footer>
+        </Card>
+      </div>
     </div>
   );
 }
