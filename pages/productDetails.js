d7c73816aa1d56653b5a8345988651a0727fb8cf
Corvu-s <roomtemp72@gmail.com>
2022-03-20 22:50:00 -0400

    fixed a bug with indexing between pages and added proper review retreval. Still need to work on posting reviews

--- pages/productDetails.js
+++ pages/productDetails.js
@@ -15,13 +15,38 @@ import NavBar from "../components/NavBar";
 export default function ProductDetails({ data }) {
   const { state, dispatch } = useAppContext();
   const [productDetails, setProductDetails] = useState(
-    state.catalogData[data.id]
+    state.catalogData[data.index]
   );
   const [reviewText, setReviewText] = useState("");
   const [reviewTitle, setReviewTitle] = useState("");
   const [reviewRating, setReviewRating] = useState(0);
+
+  useEffect(() => {
+    setProductDetails(state.catalogData[data.index]);
+  }, [state.catalogData[data.index]]);
   function handleReviewPost() {
-    console.log(reviewText + " " + reviewTitle + " " + reviewRating);
+    const review = fetch(
+      `https://eecs4413-backend-production.up.railway.app/api/reviews/product/${data.id}`,
+      {
+        method: "POST",
+        body: JSON.stringify({
+          title: reviewTitle,
+          description: reviewText,
+          rating: reviewRating,
+          product_id: data.id,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        redirect: "follow",
+      }
+    ).then((response) => {
+      return response.json();
+    });
+    console.log("test post review endpoint");
+    console.log(review);
+
+    //console.log(reviewText + " " + reviewTitle + " " + reviewRating);
     //actually post the review to the db. Consider doing a window.refresh afterwards to update the current list of reviews on screen
     alert("Review Submitted!");
     //reset the values of the review form
@@ -29,6 +54,32 @@ export default function ProductDetails({ data }) {
     setReviewRating(0);
     setReviewTitle("");
   }
+
+  function displayReviews() {
+    if (data.reviews.length == 0) {
+      //no reviews to show
+      return <p>No reviews for this product yet!</p>;
+    } else {
+      //we have reviews!
+      return data.reviews.map((review, index) => (
+        <Card key={index} bordered shadow={false} css={{ mw: "400px" }}>
+          <Card.Body css={{ p: 0 }}>
+            <Row wrap="wrap" justify="space-between">
+              <Text b>{review.title}</Text>
+
+              <Row>
+                <Text b> {review.description}</Text>
+              </Row>
+              <Row>
+                <Text> {review.rating}/5 Stars</Text>
+              </Row>
+            </Row>
+          </Card.Body>
+        </Card>
+      ));
+    }
+  }
+
   const dummyReviews = [
     {
       id: 0,
@@ -56,91 +107,80 @@ export default function ProductDetails({ data }) {
   return (
     <div>
       <NavBar />
-      <div className="flex justify-center">
-        <Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
-          <Card.Body css={{ p: 0 }}>
-            <Card.Image
-              objectFit="cover"
-              src="/placeholder-image.png"
-              width="100%"
-              height={300}
-              alt="default"
-            />
-            <Row wrap="wrap" justify="space-between">
-              <Text b>{productDetails.productName}</Text>
-              <Text b>${productDetails.price}</Text>
-              <Text css={{ color: "$accents4", fontWeight: "$semibold" }}>
-                {productDetails.quantity} Left
-              </Text>
-              <Row>
-                <Text b> Color(s): {productDetails.color}</Text>
-              </Row>
-              <Row>
-                <Text> {productDetails.description}</Text>
-              </Row>
-            </Row>
-          </Card.Body>
-          <Card.Footer>
-            <Row wrap="wrap" justify="space-between">
-              <Button
-                onClick={() =>
-                  dispatch({
-                    type: "ADD_TO_CART",
-                    //default one the selected item is added to the cart
-                    data: { item: productDetails, orderQuant: 1 },
-                  })
-                }
-              >
-                Add to cart
-              </Button>
-            </Row>
-          </Card.Footer>
-        </Card>
-      </div>
-      <h1>Reviews</h1>
-      <div className="flex">
-        <div>
-          {dummyReviews.map((review, index) => (
-            <Card key={index} bordered shadow={false} css={{ mw: "400px" }}>
+      {productDetails == undefined ? (
+        <p>loading product</p>
+      ) : (
+        <>
+          <div className="flex justify-center">
+            <Card bordered shadow={false} hoverable css={{ mw: "400px" }}>
               <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                  objectFit="cover"
+                  src="/placeholder-image.png"
+                  width="100%"
+                  height={300}
+                  alt="default"
+                />
                 <Row wrap="wrap" justify="space-between">
-                  <Text b>{review.title}</Text>
-
+                  <Text b>{productDetails.productName}</Text>
+                  <Text b>${productDetails.price}</Text>
+                  <Text css={{ color: "$accents4", fontWeight: "$semibold" }}>
+                    {productDetails.quantity} Left
+                  </Text>
                   <Row>
-                    <Text b> {review.description}</Text>
+                    <Text b> Color(s): {productDetails.color}</Text>
                   </Row>
                   <Row>
-                    <Text> {review.rating}/5 Stars</Text>
+                    <Text> {productDetails.description}</Text>
                   </Row>
                 </Row>
               </Card.Body>
+              <Card.Footer>
+                <Row wrap="wrap" justify="space-between">
+                  <Button
+                    onClick={() =>
+                      dispatch({
+                        type: "ADD_TO_CART",
+                        //default one the selected item is added to the cart
+                        data: { item: productDetails, orderQuant: 1 },
+                      })
+                    }
+                  >
+                    Add to cart
+                  </Button>
+                </Row>
+              </Card.Footer>
             </Card>
-          ))}
-        </div>
-        <div>
-          <div>
-            <Input
-              value={reviewTitle}
-              onChange={(e) => setReviewTitle(e.target.value)}
-              placeholder="Review Title"
-            />
-            <Input
-              value={reviewRating}
-              onChange={(e) => setReviewRating(e.target.valueAsNumber)}
-              placeholder="Rating"
-              type="number"
-            />
           </div>
+          <h1>Reviews</h1>
+          <div className="flex">
+            <div>{displayReviews()}</div>
+            <div>
+              <div>
+                <Input
+                  value={reviewTitle}
+                  onChange={(e) => setReviewTitle(e.target.value)}
+                  placeholder="Review Title"
+                />
+                <Input
+                  value={reviewRating}
+                  onChange={(e) => setReviewRating(e.target.valueAsNumber)}
+                  placeholder="Rating"
+                  type="number"
+                />
+              </div>
 
-          <Textarea
-            value={reviewText}
-            onChange={(e) => setReviewText(e.target.value)}
-            fullWidth={true}
-            labelPlaceholder="Write a Review"
-          />
-          <Button onClick={() => handleReviewPost()}>Post Review</Button>
-        </div>
-      </div>
+              <Textarea
+                value={reviewText}
+                onChange={(e) => setReviewText(e.target.value)}
+                fullWidth={true}
+                labelPlaceholder="Write a Review"
+              />
+              <Button onClick={() => handleReviewPost()}>Post Review</Button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 }
@@ -149,8 +189,23 @@ export async function getServerSideProps(context) {
   //add calls to the db here.
   //specifically, use this to get the reviews of this product
   const { id } = context.query;
+  const { index } = context.query;
   const { productName } = context.query;
-  const data = { id: id, productName: productName };
+
+  const reviews = await fetch(
+    `https://eecs4413-backend-production.up.railway.app/api/reviews/product/${id}`,
+    { method: "GET" }
+  ).then((res) => {
+    return res.json();
+  });
+
+  const data = {
+    id: id,
+    index: index,
+    productName: productName,
+    reviews: reviews,
+  };
   console.log(data);
+  console.log(reviews);
   return { props: { data } };
 }
