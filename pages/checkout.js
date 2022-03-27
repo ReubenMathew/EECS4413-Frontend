86fe2730708488a377ded2cee47b76753356c5f9
Nourin Abd El Hadi <nourin.abdelhadi@gmail.com>
2022-03-27 16:47:23 -0400

    Started working on checkout view

--- pages/checkout.js
+++ pages/checkout.js
@@ -1,6 +1,16 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useAppContext } from "../state/AppContext";
+import { useRouter } from "next/router";
+import { Input, Spacer, Card, Button, Text, Row } from "@nextui-org/react";
+import NavBar from "../components/NavBar";
+
 export default function Checkout() {
+  
+  
+
   return (
     <div>
+      <NavBar />
       <p>Checkout view</p>
     </div>
   );
