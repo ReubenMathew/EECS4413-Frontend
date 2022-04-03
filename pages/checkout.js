6342876572096e72da7a8e9eccf85ac380f465f4
Nourin <nourin.abdelhadi@gmail.com>
2022-04-03 01:45:27 -0400

    checkout redirects to verify
    when you press checkout, the info you input gets displayed and you have to press confirm to submit. there is also a back button to reinput the info.

--- pages/checkout.js
+++ pages/checkout.js
@@ -7,31 +7,40 @@ import NavBar from "../components/NavBar";
 
 export default function Checkout() {
   const router = useRouter();
+  const [active, setActive] = useState("true");
+
   //shipping information
   const [username, setUsername] = useState("");
-  const [billing, setAddress] = useState("");
+  const [shipping, setAddress] = useState("");
+
+  //billing information
+  const [usernameB, setBUsername] = useState("");
+  const [billing, setBAddress] = useState("");
   const [cardNum, setCardNum] = useState("");
   const [cardExp, setCardExp] = useState("");
   const [cardCSV, setCardCSV] = useState("");
 
-  //billing information
-  const [usernameB, setBUsername] = useState("");
-  const [billingB, setBAddress] = useState("");
-
   async function checkout() {
     if (
-      username == null ||
-      billing == null ||
-      cardNum == null ||
-      cardExp == null || 
-      cardCSV == null ||
-      usernameB == null ||
-      billingB == null
+      username == "" ||
+      shipping == "" ||
+      cardNum == "" ||
+      cardExp == "" ||
+      cardCSV == "" ||
+      usernameB == "" ||
+      billing == ""
     ) {
-      alert("Field cannot be empty")
-      console.log("Field cannot be empty");
+      alert("Fields cannot be empty");
+    } else {
+      alert("checkout pressed");
+      setActive("false");
     }
   }
+
+  async function back() {
+    setActive("true");
+  }
+
   return (
     <div>
       <NavBar />
@@ -44,68 +53,118 @@ export default function Checkout() {
           <Button onClick={() => router.push("/Registration")}>Register</Button>
         </div>
         <p />
-        <Card bordered shadow={false} css={{ mw: "300px" }}>
-          <Card.Body css={{ p: 0 }}>
-            <center>
-              <h3>Shipping Information</h3>
-            </center>
-            <Row wrap="wrap" justify="space-between">
-              <Row>
-                <Input
-                  placeholder="Full Name"
-                  onChange={(e) => setUsername(e.target.value)}
-                />
+        {active === "true" && (
+          <Card bordered shadow={false} css={{ mw: "300px" }} title="shipping">
+            <Card.Body css={{ p: 0 }}>
+              <center>
+                <h3>Shipping Information</h3>
+              </center>
+              <Row wrap="wrap" justify="space-between">
+                <Row>
+                  <Input
+                    placeholder="Full Name"
+                    onChange={(e) => setUsername(e.target.value)}
+                  />
+                </Row>
+                <Row>
+                  <Input
+                    placeholder="Shipping Address"
+                    onChange={(e) => setAddress(e.target.value)}
+                  />
+                </Row>
               </Row>
-              <Row>
-                <Input
-                  placeholder="Shipping Address"
-                  onChange={(e) => setAddress(e.target.value)}
-                />
+              <h3>
+                <center>Billing Information</center>
+              </h3>
+              <Row wrap="wrap" justify="space-between">
+                <Row>
+                  <Input
+                    placeholder="Full Name"
+                    onChange={(e) => setBUsername(e.target.value)}
+                  />
+                </Row>
+                <Row>
+                  <Input
+                    placeholder="Billing Address"
+                    onChange={(e) => setBAddress(e.target.value)}
+                  />
+                </Row>
+                <Row>
+                  <Input
+                    placeholder="Card Number"
+                    onChange={(e) => setCardNum(e.target.value)}
+                  />
+                </Row>
+                <Row>
+                  <Input
+                    placeholder="Expiry Date"
+                    onChange={(e) => setCardExp(e.target.value)}
+                  />
+                </Row>
+                <Row>
+                  <Input
+                    placeholder="CSV"
+                    onChange={(e) => setCardCSV(e.target.value)}
+                  />
+                </Row>
               </Row>
-            </Row>
-            <h3>
-              <center>Billing Information</center>
-            </h3>
-            <Row wrap="wrap" justify="space-between">
+            </Card.Body>
+            <Card.Footer>
               <Row>
-                <Input
-                  placeholder="Full Name"
-                  onChange={(e) => setBUsername(e.target.value)}
-                />
+                <div>
+                  <Button onClick={() => checkout()}>Checkout</Button>
+                </div>
               </Row>
-              <Row>
-                <Input
-                  placeholder="Billing Address"
-                  onChange={(e) => setBAddress(e.target.value)}
-                />
-              </Row>
-              <Row>
-                <Input
-                  placeholder="Card Number"
-                  onChange={(e) => setCardNum(e.target.value)}
-                />
-              </Row>
-              <Row>
-                <Input
-                  placeholder="Expiry Date"
-                  onChange={(e) => setCardExp(e.target.value)}
-                />
-              </Row>
-              <Row>
-                <Input
-                  placeholder="CSV"
-                  onChange={(e) => setCardCSV(e.target.value)}
-                />
-              </Row>
-            </Row>
-          </Card.Body>
-          <Card.Footer>
-            <Row>
-              <Button onClick={() => checkout()}>Checkout</Button>
-            </Row>
-          </Card.Footer>
-        </Card>
+            </Card.Footer>
+          </Card>
+        )}
       </center>
+      {active === "false" && (
+        <center>
+          <Card bordered shadow={false} css={{ mw: "300px" }} title="verify">
+            <Card.Body css={{ p: 0 }}>
+              <center>
+                <h3>Shipping Information</h3>
+              </center>
+              <p>
+                <b>Full Name: </b>
+                {username}
+                <p />
+                <b>Shipping Address: </b>
+                {shipping}
+                <p />
+              </p>
+              <h3>
+                <center>Billing Information</center>
+              </h3>
+              <p>
+                <b>Full Name: </b>
+                {usernameB}
+                <p />
+                <b>Billing Address: </b>
+                {billing}
+                <p />
+                <b>Card Number:</b> {cardNum}
+                <p />
+                <b>Expiry Date: </b>
+                {cardExp}
+                <p />
+                <b> CSV: </b>
+                {cardCSV}
+              </p>
+            </Card.Body>
+            <Card.Footer>
+              <Row>
+                <div>
+                  <Button>Confirm</Button>
+                  <br />
+                  <Button onClick={() => back()}>Back</Button>
+                </div>
+              </Row>
+            </Card.Footer>
+          </Card>
+        </center>
+      )}
     </div>
   );
 }
