68df4d227d239e275129db63c42701a3a2eacb3b
Nourin <nourin.abdelhadi@gmail.com>
2022-04-02 22:38:26 -0400

    Added all the required fields for checkout

--- pages/checkout.js
+++ pages/checkout.js
@@ -3,47 +3,109 @@ import { useAppContext } from "../state/AppContext";
 import { useRouter } from "next/router";
 import { Input, Spacer, Card, Button, Text, Row } from "@nextui-org/react";
 import NavBar from "../components/NavBar";
+// import DatePicker from 'react-date-picker'
 
 export default function Checkout() {
   const router = useRouter();
+  //shipping information
+  const [username, setUsername] = useState("");
+  const [billing, setAddress] = useState("");
+  const [cardNum, setCardNum] = useState("");
+  const [cardExp, setCardExp] = useState("");
+  const [cardCSV, setCardCSV] = useState("");
 
+  //billing information
+  const [usernameB, setBUsername] = useState("");
+  const [billingB, setBAddress] = useState("");
+
+  async function checkout() {
+    if (
+      username == null ||
+      billing == null ||
+      cardNum == null ||
+      cardExp == null || 
+      cardCSV == null ||
+      usernameB == null ||
+      billingB == null
+    ) {
+      alert("Field cannot be empty")
+      console.log("Field cannot be empty");
+    }
+  }
   return (
     <div>
       <NavBar />
       <h1>Checkout</h1>
-      <div>
-        <Button onClick={() => router.push("/login")}>Login</Button>
-        <Button onClick={() => router.push("/Registration")}>Register</Button>
-      </div>
-      <Card bordered shadow={false} css={{ mw: "300px" }}>
-        <Card.Body css={{ p: 0 }}>
-          <Row wrap="wrap" justify="space-between">
-            <Row>
-              <Input
-                placeholder="Username"
-                onChange={(e) => setUsername(e.target.value)}
-              />
+      <center>
+        {" "}
+        <div>
+          <Button onClick={() => router.push("/login")}>Login</Button>
+          <p />
+          <Button onClick={() => router.push("/Registration")}>Register</Button>
+        </div>
+        <p />
+        <Card bordered shadow={false} css={{ mw: "300px" }}>
+          <Card.Body css={{ p: 0 }}>
+            <center>
+              <h3>Shipping Information</h3>
+            </center>
+            <Row wrap="wrap" justify="space-between">
+              <Row>
+                <Input
+                  placeholder="Full Name"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </Row>
+              <Row>
+                <Input
+                  placeholder="Shipping Address"
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </Row>
             </Row>
-            <Row>
-              <Input
-                placeholder="Email"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+            <h3>
+              <center>Billing Information</center>
+            </h3>
+            <Row wrap="wrap" justify="space-between">
+              <Row>
+                <Input
+                  placeholder="Full Name"
+                  onChange={(e) => setBUsername(e.target.value)}
+                />
+              </Row>
+              <Row>
+                <Input
+                  placeholder="Billing Address"
+                  onChange={(e) => setBAddress(e.target.value)}
+                />
+              </Row>
+              <Row>
+                <Input
+                  placeholder="Card Number"
+                  onChange={(e) => setCardNum(e.target.value)}
+                />
+              </Row>
+              <Row>
+                <Input
+                  placeholder="Expiry Date"
+                  onChange={(e) => setCardExp(e.target.value)}
+                />
+              </Row>
+              <Row>
+                <Input
+                  placeholder="CSV"
+                  onChange={(e) => setCardCSV(e.target.value)}
+                />
+              </Row>
             </Row>
+          </Card.Body>
+          <Card.Footer>
             <Row>
-              <Input
-                placeholder="Password"
-                onChange={(e) => setPassword(e.target.value)}
-              />
+              <Button onClick={() => checkout()}>Checkout</Button>
             </Row>
-          </Row>
-        </Card.Body>
-        <Card.Footer>
-          <Row>
-            <Button onClick={() => handleRegister()}>Register</Button>
-          </Row>
-        </Card.Footer>
-      </Card>
+          </Card.Footer>
+        </Card>
+      </center>
     </div>
   );
 }
